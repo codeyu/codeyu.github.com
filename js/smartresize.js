bca740f2e502c938494d72a7f9314ee8717f2ce3
codeyu <codeyu@gmail.com>
2023-08-12 16:21:37 +0000

    Site updated: 2023-08-12 16:21:37

--- js/smartresize.js
+++ js/smartresize.js
@@ -1,1 +1,1 @@
-+function(n,r){var t=function(n,r,t){var e;return function(){function i(){t||n.apply(u,a),e=null}var u=this,a=arguments;e?clearTimeout(e):t&&n.apply(u,a),e=setTimeout(i,r||100)}};jQuery.fn[r]=function(n){return n?this.bind("resize",t(n)):this.trigger(r)}}(jQuery,"smartresize");
+!function(i,t){jQuery.fn[t]=function(i){return i?this.bind("resize",(e=i,function(){var i=this,t=arguments;u?clearTimeout(u):r&&e.apply(i,t),u=setTimeout((function(){r||e.apply(i,t),u=null}),n||100)})):this.trigger(t);var e,n,r,u}}(jQuery,"smartresize");
--- js/totop.js
+++ js/totop.js
@@ -1,1 +1,1 @@
-$(window).scroll(function(){$("#rocket").toggleClass("show",window.pageYOffset>50)}),$("#rocket").click(function(){return $("#rocket").addClass("launch"),$("html, body").animate({scrollTop:0},500,function(){$("#rocket").removeClass("show launch")}),!1});
+$(window).scroll((function(){$(window).scrollTop()>500?$("#rocket").addClass("show"):$("#rocket").removeClass("show")})),$("#rocket").click((function(){return $("#rocket").addClass("launch"),$("html, body").animate({scrollTop:0},500,(function(){$("#rocket").removeClass("show launch")})),!1}));
